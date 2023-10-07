5139fbe422634f32d674f7a583e320d5827d4037
Lakshay Kamat <lakshaykamat2048@gmail.com>
2023-10-07 22:38:50 +0530

    Handle email input exception

--- email_slicer.py
+++ email_slicer.py
@@ -1,5 +1,14 @@
-email = input("enter your email: ")
-index = email.index("@")
-username = email[:index]
-domain = email[index+1:]
-print(f"your useername is {username} and domain is {domain}")
+def get_username(email):
+    if '@' in email:
+        email_index = email.index('@')
+        username = email[:email_index]
+        return username
+
+def get_domain(email):
+    if '@' in email:
+        email_index = email.index('@')
+        return email[email_index+1:]
+
+
+email = input("Enter email:")
+print(f"Username:{get_username(email)}\nDomain:{get_domain(email)}")
